f84ef6e7daeb5a008c7d3459f269e215b13989ec
Alex Silcock <alex@alexsilcock.net>
2014-09-08 00:30:05 +0100

    Prevent infinite loops of event firing when calling activate() on a Cell from outside the Cell

--- server/public/Cell.js
+++ server/public/Cell.js
@@ -5,52 +5,56 @@ var Cell = function(x, y) {
     this.element.get(0).jsObj = this; // a bit hacky but it allows us to get a reference to the Cell from the DOM elem
 
     this.element.get(0).addEventListener('mousedown', function() {
-        this.activate();
+        this.activate(true);
     }.bind(this));
 
     this.element.get(0).addEventListener('mouseup', function() {
-        this.deactivate();
+        this.deactivate(true);
     }.bind(this));
 
     this.element.get(0).addEventListener('mouseover', function (event) {
         if (event.which != 0) {
             // user is dragging
-            this.activate();
+            this.activate(true);
         }
     }.bind(this));
 
     this.element.get(0).addEventListener('mouseout', function (event) {
         if (this.active) {
-            this.deactivate();
+            this.deactivate(true);
         }
     }.bind(this));
 
-    this.bind('tdActivate', this.activate.bind(this));
-    this.bind('tdDeactivate', this.deactivate.bind(this));
+    this.bind('tdActivate', this.activate.bind(this, true));
+    this.bind('tdDeactivate', this.deactivate.bind(this, true));
 }
 
 // The cell was pressed on/dragged over/clicked on. Basically, the event that
 // should make the cell light up because it is being interacted with.
 // Called by TouchDrag
-Cell.prototype.activate = function() {
+Cell.prototype.activate = function(refireEvent) {
     if (this.active) return;
 
     this.active = true;
     this.element.addClass('active');
 
     // Refire the event for the Grid class to listen to
-    this.trigger('activate', this.coord);
+    if (refireEvent) {
+        this.trigger('activate', this.coord);
+    }
 }
 
 // The cell has stopped being dragged over/touched. Called by TouchDrag
-Cell.prototype.deactivate = function() {
+Cell.prototype.deactivate = function(refireEvent) {
     if (!this.active) return;
 
     this.active = false;
     this.element.removeClass('active');
 
     // Refire the event for the Grid class to listen to
-    this.trigger('deactivate', this.coord);
+    if (refireEvent) {
+        this.trigger('deactivate', this.coord);
+    }
 }
 
 MicroEvent.mixin(Cell);
